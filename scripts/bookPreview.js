469f9658246bf8b95d43b567e8b275d70a0cab04
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-09-15 21:26:45 +0200

    updated bookPreview

--- scripts/bookPreview.js
+++ scripts/bookPreview.js
@@ -2,19 +2,22 @@ let cover = document.createElement("img");
 cover.src = sessionStorage.getItem("imgSrc");
 cover.className = "cover";
 
-let author = document.createElement("p");
-author.innerHTML = sessionStorage.getItem("author");
-author.className = "author";
-
 let title = document.createElement("p");
 title.innerHTML = sessionStorage.getItem("title");
 title.className = "title";
 
+let author = document.createElement("p");
+author.innerHTML = "By: ";
+author.innerHTML += sessionStorage.getItem("author");
+author.className = "author";
+
+
 let catg = document.createElement("p");
-catg.innerHTML = sessionStorage.getItem("catg");
+catg.innerHTML = "Category: "
+catg.innerHTML += sessionStorage.getItem("catg");
 catg.className = "category"
 
 document.body.appendChild(cover);
-document.body.appendChild(author);
 document.body.appendChild(title);
+document.body.appendChild(author);
 document.body.appendChild(catg);
