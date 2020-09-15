5a45b212af433b12c9a8a746c169f36660537837
v01dc0d3 <at200000000@gmail.com>
2020-09-15 23:13:09 +0200

    Added Admin account "kotobna@k.k"

---
+++ scripts/admin.js
@@ -0,0 +1,7 @@
+let orange = document.getElementById("btn1");
+let yellow = document.getElementById("btn2");
+let grey = document.getElementById("btn3");
+
+let small = document.getElementById("btn4");
+let med = document.getElementById("btn5");
+let large = document.getElementById("btn6");
--- scripts/loggedChk.js
+++ scripts/loggedChk.js
@@ -1,11 +1,15 @@
 let logI = document.getElementById("loginNav");
 let logO = document.getElementById("logoutNav");
+let admin = document.getElementById("adminNav");
 
 if(sessionStorage.getItem("logged"))
 {
     logI.parentNode.removeChild(logI);
+    if(sessionStorage.getItem("logged") != "kotobna@k.k")
+        admin.parentNode.removeChild(admin);
 }
 else
 {
     logO.parentNode.removeChild(logO);
+    admin.parentNode.removeChild(admin);
 }
