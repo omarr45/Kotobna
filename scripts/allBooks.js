6d15e6b81608dd4d5e5bbd2e9c7f2fffbc2e0a09
v01dc0d3 <at200000000@gmail.com>
2020-09-15 15:56:25 +0200

    Added Books

--- scripts/allBooks.js
+++ scripts/allBooks.js
@@ -23,7 +23,6 @@ class Book {
         eCategory.className = "category";
         
         var figcap = document.createElement("figcaption");
-        figcap.className = "container";
         figcap.appendChild(eAuthor);
         figcap.appendChild(eTitle);
         figcap.appendChild(eCategory);
@@ -48,7 +47,7 @@ class Books {
 
         for (let i = 0; i < this.books.length; i++) {
             const element = this.books[i];
-            if (category == "all" || element.category == category)
+            if (category == "All Books" || element.category == category)
                 element.show(this.container);
         }
     }
@@ -60,7 +59,13 @@ class Books {
 let library = new Books();
 
 //Book list
-for (let i = 0; i < 20; i++)
+for (let i = 0; i < 9; i++) {
     library.books.push(new Book("A Brief History of Time", "Stephen Hawking", "Science", "assets/covers/BriefHistoryOfTime.jpg"));
+    library.books.push(new Book("Harry Potter And The Order Of The Phoenix", "JK, ROWLING", "Novels", "assets/covers/banners/harry_potter_and the_order_of_the_phoenix_book.jpeg"));
+    library.books.push(new Book("أولاد حارتنا", "NAGIB MAHFOUZ", "Novels", "assets/covers/standard/awlad_haretna.jpg"));
+    library.books.push(new Book("The Da vinci Code", "DAN BROWN", "Novels", "assets/covers/standard/the_da_vinci_code.jpg"));
+    library.books.push(new Book("The Great Gatspy", "SCOTT FITZGERALD", "Novels", "assets/covers/standard/the_great_gatspy.jpg"));
+    library.books.push(new Book("Me And A Guy Named Elvis Presly", "JERRY SCHILLING", "Biography", "assets/covers/standard/me_and_a_guy_named_elvis_presly.jpg"));
+}
 ///////////////////////
-library.show("all");
+library.show("All Books");
--- scripts/register.js
+++ scripts/register.js
@@ -1,25 +1,41 @@
-function registeration() {
+function Validation() {
+    
+    var val=true;
+
     if(document.regisForm.fname.value == "") {
         alert("First Name Can not be EMPTY !!");
+        val=false;
     }
     else if(document.regisForm.sname.value == "") {
         alert("Second Name Can not be EMPTY !!");
+        val=false;
     }
     else if(document.regisForm.Email.value == ""){
         alert("E-mail Can not be EMPTY !!");
+        val=false;
     }
     else if(document.regisForm.Email.value.indexOf("@") == -1 || document.regisForm.Email.value.indexOf(".") == -1)
     {
         alert("Please Enter E-mail Format eg: jackie@gmail.com");
+        val=false;
     }
     else if(document.regisForm.pass.value == "" || document.regisForm.passConfirm.value == ""){
         alert("The Password Can not be EMPTY !!");
+        val=false;
     }
     else if((document.regisForm.pass.value !== document.regisForm.passConfirm.value)) {
-        alert("The Password Does Not MATCH !!");
+        alert("The Password Does not MATCH !!");
+        val=false;
     }
-    else {
+    return val;
+}
+
+function register()
+{
+    if(Validation())
+    {
         alert("You Have Registered Successfully");
+        sessionStorage.setItem("logged", document.regisForm.Email.value);
         window.location.href = "home.html";
     }
 }
