c0ae3b1532606692369e062219d74a615caecb37
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-09-16 03:37:23 +0200

    updated formatting

--- scripts/loggedChk.js
+++ scripts/loggedChk.js
@@ -2,14 +2,11 @@ let logI = document.getElementById("loginNav");
 let logO = document.getElementById("logoutNav");
 let admin = document.getElementById("adminNav");
 
-if(sessionStorage.getItem("logged"))
-{
+if (sessionStorage.getItem("logged")) {
     logI.parentNode.removeChild(logI);
-    if(sessionStorage.getItem("logged") != "kotobna@k.k")
+    if (sessionStorage.getItem("logged") != "kotobna@k.k")
         admin.parentNode.removeChild(admin);
-}
-else
-{
+} else {
     logO.parentNode.removeChild(logO);
     admin.parentNode.removeChild(admin);
 }
--- scripts/login.js
+++ scripts/login.js
@@ -1,28 +1,24 @@
-function Validation(){
+function Validation() {
     var validation = true;
-    if(document.logForm.Email.value == ""){
+    if (document.logForm.Email.value == "") {
         validation = false;
         alert("E-mail Can not be EMPTY !!");
-    }
-    else if(document.logForm.Email.value.indexOf("@") == -1 || document.logForm.Email.value.indexOf(".") == -1)
-    {
+    } else if (document.logForm.Email.value.indexOf("@") == -1 || document.logForm.Email.value.indexOf(".") == -1) {
         validation = false;
         alert("Please Enter E-mail Format ex: jackie@gmail.com");
-    }
-    else if(document.logForm.Password.value == ""){
+    } else if (document.logForm.Password.value == "") {
         validation = false;
         alert("Password Can not be EMPTY !!");
     }
     return validation;
 }
-function Registeration(){
+
+function Registeration() {
     window.location.href = 'register.html';
 }
 
-function login()
-{
-    if(Validation())
-    {
+function login() {
+    if (Validation()) {
         sessionStorage.setItem("logged", document.logForm.Email.value);
         window.location.href = "home.html";
     }
