eae80f9eebb179e53f952b10d1f33646f51ced9d
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-09-17 19:25:07 +0200

    update bestSellers links

--- scripts/bestSellers.js
+++ scripts/bestSellers.js
@@ -67,9 +67,9 @@ class Books {
 let library = new Books();
 ////Best 5 Seller Books
 library.books.push(new Book("A Brief History of Time", "Stephen Hawking", "Science", "assets/covers/bhot.jpg", "https://www.goodreads.com/book/show/3869.A_Brief_History_of_Time", "https://www.fisica.net/relatividade/stephen_hawking_a_brief_history_of_time.pdf"));
-library.books.push(new Book("The Da Vinci Code", "Dan Brown", "Novels", "assets/covers/tdvc.jpg"));
-library.books.push(new Book("Murder On The Orient Express", "Agatha Christie", "Novels", "assets/covers/motoe.jpg"));
-library.books.push(new Book("Infinite Powers", "Steven Strogatz", "Science", "assets/covers/ip.jpg"));
+library.books.push(new Book("The Da Vinci Code", "Dan Brown", "Novels", "assets/covers/tdvc.jpg", "https://www.goodreads.com/book/show/30228538-the-da-vinci-code?from_search=true&from_srp=true&qid=k64zPgwqtg&rank=3", "https://danbrown.com/wp-content/themes/danbrown/assets/db_dvc_book_excerpts.pdf"));
+library.books.push(new Book("Murder On The Orient Express", "Agatha Christie", "Novels", "assets/covers/motoe.jpg", "https://www.goodreads.com/book/show/853510.Murder_on_the_Orient_Express?from_search=true&from_srp=true&qid=6BycuUGXxO&rank=1", "http://detective.gumer.info/anto/christie_8_2.pdf"));
+library.books.push(new Book("Infinite Powers", "Steven Strogatz", "Science", "assets/covers/ip.jpg", "https://www.goodreads.com/book/show/40796176-infinite-powers?from_search=true&from_srp=true&qid=pPYPjDJgnE&rank=1", "https://www.slideshare.net/kezyhuro89502/2019-infinite-powers-pdf-how-calculus-reveals-the-secrets-of-the-universe-by-steven-strogatz-houghton-mifflin-harcourt"));
 /////
 library.show();
 
@@ -79,7 +79,7 @@ document.getElementById("hBanner").onclick = function() {
     sessionStorage.setItem("author", "J.K. Rowling");
     sessionStorage.setItem("title", "Harry Potter And The Order Of The Phoenix");
     sessionStorage.setItem("catg", "Novels");
-    sessionStorage.setItem("read", "");
-    sessionStorage.setItem("rate", "");
+    sessionStorage.setItem("read", "https://www.missskirtich.com/uploads/2/3/3/7/23374820/5_-_harry_potter_and_the_order_of_the_phoenix_chapter_37.pdf");
+    sessionStorage.setItem("rate", "https://www.goodreads.com/book/show/1317181.Harry_Potter_and_the_Order_of_the_Phoenix?from_search=true&from_srp=true&qid=yIo5kQ6aD8&rank=2");
     window.location.href = "bookPreview.html";
 }
