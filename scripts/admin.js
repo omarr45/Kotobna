6fdfe30d3557370b67b3c8e6b408c2dd0c64e11e
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-09-20 22:01:29 +0200

    updated stuff

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,10 +1,11 @@
-
  function doBeige() {
-   document.body.style.background = "#ffebb7";
+     document.body.style.background = "#ffebb7";
  }
+
  function doGrey() {
-   document.body.style.background = "#666";
+     document.body.style.background = "#666";
  }
+
  function doOriginal() {
-   document.body.style.background = "#f1e5d9";
+     document.body.style.background = "#f1e5d9";
  }
