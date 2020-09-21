682ea70fd7439a88933276dae88aa8645744f9a3
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-09-21 22:09:08 +0200

    fixed misspelling

--- scripts/home.js
+++ scripts/home.js
@@ -17,7 +17,7 @@ function startSliding() {
 
 function validateEmail(email) {
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailPattern.test(elementValue);
+    return emailPattern.test(email);
 }
 
 function Submit() {
